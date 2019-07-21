91dd9adef9efd31b2f807876372d9f02efdeb778
Matthew Bunday <mkbunday@gmail.com>
2019-07-20 23:48:05 -0400

    Remove translation code I don't need, change footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ class Footer extends React.Component {
           </a>
         </div>
         <a
-          href="https://mobile.twitter.com/dan_abramov"
+          href="https://mobile.twitter.com/zencephalon"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -25,19 +25,15 @@ class Footer extends React.Component {
         </a>{' '}
         &bull;{' '}
         <a
-          href="https://github.com/gaearon"
+          href="https://github.com/zencephalon"
           target="_blank"
           rel="noopener noreferrer"
         >
           github
         </a>{' '}
         &bull;{' '}
-        <a
-          href="https://stackoverflow.com/users/458193/dan-abramov"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          stack overflow
+        <a href="zencephalon.com" target="_blank" rel="noopener noreferrer">
+          zencephalon
         </a>
       </footer>
     );
